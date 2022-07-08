836669a10a5861e414ca709b68e08c27fbf1fb46
ErtyumPX <ertyum35@gmail.com>
2022-07-08 14:54:40 +0300

    testing

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 console.log("Main Initialized")
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
+
+
+
+
+document.getElementById('share-button').addEventListener('click', function () {
+    FB.ui({
+        method: 'share',
+        href: "https://en.wikipedia.org/wiki/Main_Page"
+    }, function (response) {
+        if (!response) {
+            console.log('User did not share the page.');
+        }
+        else {
+            console.log('User shared the page!');
+        }
+    });
 });
+
